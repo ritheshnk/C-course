2c649dd36aa10d546fd510f4c6b174b7bf06446b
Rithesh NK <61980882+ritheshnk@users.noreply.github.com>
2022-07-10 19:52:11 +0530

    Add files via upload

---
+++ divisibleby2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+  /*int a;
+  printf("enter a num :");
+  scanf("%d",&a);
+  if(a%2==0)
+  {
+    printf("the number is divible by 2");
+  }
+  else
+  {
+    printf("not divisible");
+  }
+  */
+
+  int a;
+  printf("enter a num :");
+  scanf("%d",&a);
+  printf("%d",a%2==0);
+}
