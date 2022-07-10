b8e97d29ae4600f22dda9bc3038844ec7c403b77
Rithesh NK <61980882+ritheshnk@users.noreply.github.com>
2022-07-10 19:51:55 +0530

    Add files via upload

---
+++ circle.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include<math.h>
+#define PI 3.14;
+
+int main(void) {
+  //area of a square
+  float radius,A;
+ printf("enter the radius:");
+  scanf("%f",&radius);
+  A=PI*radius*radius;
+  printf("area of circle is =%f",A);
+  }
+
+  /*
+  int main(){
+  float r;
+  printf("enter the value of radius;");
+  scanf("%f",&r);
+  printf("area is :%f",3.14 * r * r);
+  
+}
+*/
