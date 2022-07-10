db9cffdf5161147c84fca323f479ad5f60c01394
Rithesh NK <61980882+ritheshnk@users.noreply.github.com>
2022-07-10 19:51:39 +0530

    Add files via upload

---
+++ checkconditionusingprint.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include<math.h>
+int main()
+{
+ 
+ /* if(3!=3 || 2!=2)
+  {
+    printf("condition is true");
+    
+  }
+  else 
+  printf("condtion is false"); */
+
+
+
+
+
+
+ printf("%d\n",4<3 || 5==2);
+  printf("%d\n",4>3 && 5!=2);
+  
+  /*we are giving %d inside printf because the value is either true or false which is an integer (1 or 0) hence we use %d*/
+  return 0;
+
+
+}
