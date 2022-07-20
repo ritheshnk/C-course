db878f665e5664851439675549809b52303e1e56
Rithesh NK <61980882+ritheshnk@users.noreply.github.com>
2022-07-20 22:50:37 +0530

    Add files via upload

---
+++ nomultipleof7.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+int n;
+  do
+    {
+      printf("enter a num: \n");
+      scanf("%d",&n);
+      printf("the num is %d \n",n);
+
+    if(n%7 == 0)
+    {
+      break;
+    }
+    }while(1);
+  printf("\nthank you")
+;
+  }
