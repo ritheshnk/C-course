84b013ac1d4faaf5e3ad3ab9de20d20e18180b1f
Rithesh NK <61980882+ritheshnk@users.noreply.github.com>
2022-08-23 20:54:58 +0530

    Add files via upload

---
+++ hellongoodbyusingfuntion.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+void greeting();
+int main()
+{
+  greeting();
+  greeting(); // printing more than once funtion is used 
+return 0;
+}
+void greeting()
+{
+  printf("hello boi!!!\n");
+  printf("welcome toc programming\n");
+}
