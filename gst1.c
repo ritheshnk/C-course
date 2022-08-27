41a7a0a6f952cc307742399c1bb47ecbc658665b
Rithesh NK <61980882+ritheshnk@users.noreply.github.com>
2022-08-27 19:19:20 +0530

    Update gst1.c

--- gst1.c
+++ gst1.c
@@ -4,12 +4,10 @@
 int main()
 {
   float price[3];
-  printf("enter the price of item 1:");
-  scanf("%f\n",&price[0]);
-  printf("enter the price of item 2:");
-  scanf("%f\n",&price[1]);
-  printf("enter the price of item 3:");
-  scanf("%f\n",&price[2]);
+  printf("enter the price of 3 items:");
+  scanf("%f",&price[0]);
+  scanf("%f",&price[1]);
+  scanf("%f",&price[2]);
   printf("the price of item 1 with gst:%f\n",price[0]+price[0]*0.18);
   printf("the price of item 1 with gst:%f\n",price[1]+price[1]*0.18);
   printf("the price of item 1 with gst:%f\n",price[2]+price[2]*0.18);
