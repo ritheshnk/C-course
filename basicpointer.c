b1cc17880b16e113404b9597652f846700ded120
Rithesh NK <61980882+ritheshnk@users.noreply.github.com>
2022-08-27 18:58:02 +0530

    Add files via upload

---
+++ basicpointer.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+  int marks[3];
+  printf("enter sub1 marks:");
+  scanf("%d/n",&marks[0]);
+    printf("enter sub2 marks:");
+  scanf("%d/n",&marks[1]);
+    printf("enter sub3 marks:");
+  scanf("%d/n",&marks[2]);
+  printf("sub1:%d,sub2:%d,sub3:%d",marks[0],marks[1],marks[2]);
+  return 0;
+}
